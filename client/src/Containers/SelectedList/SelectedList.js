3c32b4fe62c1c93b7a3f8da07fc65b3a7ea001d3
Roman Duda <roman.duda@80000hours.org>
2021-04-25 14:44:57 +0100

    feat(client): add list menu

--- client/src/Containers/SelectedList/SelectedList.js
+++ client/src/Containers/SelectedList/SelectedList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectCurrentList } from '../AllLists/allListsSlice';
 import { Section } from '../../Components/Section/Section';
+import { ListMenu } from '../../Components/ListMenu/ListMenu';
 
 export function SelectedList() {
   const currentList = useSelector(selectCurrentList);
@@ -23,7 +24,10 @@ export function SelectedList() {
 
   return (
     <div className="SelectedList">
-      <h1>{currentList ? currentList.title : '' }</h1>
+      <div className="SelectedList__header">
+        <h1>{currentList ? currentList.title : '' }</h1>
+        <ListMenu />
+      </div>
       {sections}
     </div>
   );
