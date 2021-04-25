bb855e8d4a23c380a5dcc2c0d38082e2bd02d579
Roman Duda <roman.duda@80000hours.org>
2021-04-25 15:14:57 +0100

    feat(server): adding section returns list populated with tasks

--- server/controllers/sectionController.js
+++ server/controllers/sectionController.js
@@ -8,8 +8,18 @@ async function addSection(req, res) {
     const list = await currUser.lists.id(listId);
     list.sections = [...list.sections, { title: req.body.title }];
     const updatedUser = await currUser.save();
+    const populatedUser = await updatedUser.populate({
+      path: 'lists',
+      populate: {
+        path: 'sections',
+        populate: {
+          path: 'tasks',
+        },
+      },
+    })
+      .execPopulate();
     res.status(201);
-    res.send(updatedUser);
+    res.send(populatedUser.lists);
   } catch (error) {
     res.status(400);
     res.send({ error, message: 'Could not add section' });
