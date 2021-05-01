c1e55c996cfb2d9e7aff8fba062fb020978e37e7
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-01 09:20:49 +0200

    test: task controller set up

---
+++ server/controllers/taskController.test.js
@@ -0,0 +1,24 @@
+const {
+  addNewTask,
+  addExistingTask,
+  updateTask,
+  deleteTask,
+
+} = require('./userController');
+
+describe('addNewTask', () => {
+  it('should add a new task and return user lists', async () => {
+  });
+});
+describe('addExistingTask', () => {
+  it('should add a new task by updating the lists and sections and return user lists', async () => {
+  });
+});
+describe('updateTask', () => {
+  it('should update a task and return user lists', async () => {
+  });
+});
+describe('deleteTask', () => {
+  it('should delete a task, remove the section and return the updated user', async () => {
+  });
+});
