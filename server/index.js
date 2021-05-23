dacb33d2db222c17b6a1a54d5c3041caf02aa510
Roman Duda <romanduda@gmail.com>
2021-05-23 13:03:17 +0100

    chore(client): on server start log port listening on to console

--- server/index.js
+++ server/index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`); // eslint-disable-line
+  console.log(`Server listening on port: ${PORT}`); // eslint-disable-line
 });
