b28bc0977deacb1509448e1ee98b574e91e7bacc
Roman Duda <romanduda@gmail.com>
2021-06-06 19:37:53 +0100

    refactor(client): move AuthWrap from App.js to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { AllLists } from './Containers/AllLists/AllLists';
 import { SelectedList } from './Containers/SelectedList/SelectedList';
-import { AuthWrap } from './Containers/AuthWrap/AuthWrap';
 import { Login } from './Containers/Login/Login';
 import { selectAuthState } from './Containers/AuthWrap/authSlice';
 import './App.css';
@@ -18,12 +17,10 @@ function App() {
   );
   const unauthenticatedApp = (<Login />);
   return (
-    <AuthWrap>
-      <div className="App">
-        { user ? authenticatedApp : unauthenticatedApp}
-        <footer />
-      </div>
-    </AuthWrap>
+    <div className="App">
+      { user ? authenticatedApp : unauthenticatedApp}
+      <footer />
+    </div>
   );
 }
 
